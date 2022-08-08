53c7e0d22360606b29a0ce6f503bb47096afc6e7
ehabdev <108612502+ehabdev@users.noreply.github.com>
2022-08-08 12:00:59 +0300

    Add files via upload

---
+++ K8S_backend-testvalidation.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import socket
+
+def verifyfront(userid):
+    f = open("C:\\Users\\idavos\\k8s_url.txt", "r")
+    furl=f.readline()
+    furle=furl.strip()+'/users/'
+    url=str(furle)+str(userid)
+    path='C:\\Users\\idavos\\Desktop\\Devopscourse\\chromedriver.exe'
+
+
+    try:
+        driver = webdriver.Chrome(executable_path=path)
+        driver.implicitly_wait(5)
+        driver.get(url)
+        driver.maximize_window()
+
+
+        pslocal=driver.page_source
+        if str(pslocal) != None :
+            print(pslocal)
+        else :
+            print("failed to retrive the user")
+
+
+
+    except Exception as e :
+            print("test failed")
+
+    finally:
+        driver.quit()
+
+
+verifyfront(2)
